e500135d0cb5ba0e4e642b04f7419bc149c6f4ce
Makoto <matheus.mkodama@gmail.com>
2021-09-02 22:37:04 -0300

    final da aula

---
+++ recebe.php
@@ -0,0 +1,12 @@
+<?php
+
+//echo"recebe.php";
+
+//var_dump($_POST);
+
+echo "o seu email é ". $_POST['email'];
+
+echo"seu email é {$_POST['email']}";
+
+
+
--- vetor.php
+++ vetor.php
@@ -39,3 +39,61 @@ $semana['domingo'] = "descançar" ;
 foreach($semana as $dia => $atividade){
     echo "Dia: $dia => $atividade<br>";
 }
+
+
+$grade['segunda'] = "PI-2";
+$grade['terca'] = "CMS";
+$grade['quarta'] = "bd";
+$grade['quinta'] = "LS";
+$grade['sexta'] = "LSW";
+
+echo"<pre>";//tira a formataçao do texto
+
+var_dump($grade);//ajuda na depuraçao
+
+
+
+foreach($grade as $dia => $aula){
+    echo"Dia : $dia => $aula\n";
+}
+
+echo"um tab é \t";
+echo"uma quebra de linha em Windows é \r\n \n";
+echo"se quiser mostrar aspas \"tudo bem\"";
+echo"</pre>";
+
+unset($grade);
+unset($dia);
+unset($aula);
+$grade['segunda'][0] = "PI-2";
+$grade['terca'][0] = "CMS";
+$grade['terca'][1]= "direito";
+$grade['quarta'][0] = "bd";
+$grade['quinta'][0] = "LS";
+$grade['sexta'][0] = "LSW";
+
+echo"<pre>";
+
+var_dump($grade);
+
+
+
+
+foreach($grade as $dia => $materias){
+
+    echo"Aula(s) na $dia\n";
+
+    foreach($materias as $materia){
+        echo"$materia, ";
+    }
+
+    echo"\n";
+
+}
+
+
+
+echo"</pre>";
+
+
+include('linkform.html');
