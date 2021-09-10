d646523212d4501ae2f219a281c6ffacab8873df
Makoto <matheus.mkodama@gmail.com>
2021-09-09 22:29:44 -0300

    fim da aula

---
+++ gravaDisciplina.php
@@ -0,0 +1,28 @@
+<?php
+
+echo"Disciplina: {$_POST['nomeAula']}";
+echo"<br><br>";
+echo"Professor: {$_POST['nomeProfessor']}";
+echo"<br><br>";
+echo"Dia da disciplina: {$_POST['diaAula']}";
+echo"<br><br>";
+echo"Descriçao: {$_POST['descricao']}";
+echo"<br><br>";
+
+//abre arquivo
+$file = fopen('BD.csv','a');
+//grava no arquivo
+fwrite($file, "{$_POST['nomeAula']};{$_POST['nomeProfessor']};{$_POST['diaAula']};{$_POST['descricao']};{$SERVER['REMOTE_ADDR']}\r\n");
+//fecha arquivo
+fclose($file);
+
+
+echo"<br><br> {$_POST['nomeAula']} gravada com sucesso do IP {$SERVER['REMOTE_ADDR']}!";
+
+//FORMATACAO DA DATA
+//forma 1
+// $nomeDaVar = substr($nomeDaVar,8,2)."/".substr($nomeDaVar,5,2)."/".substr($nomeDaVar,0,4);
+
+//forma2
+//$timestamp = strtotime($nomeDaVar);
+//$nomeDaVar = date('d/m/Y',$timestamp);
--- recebe.php
+++ recebe.php
@@ -3,10 +3,13 @@
 //echo"recebe.php";
 
 //var_dump($_POST);
+$_POST['email'] = $_POST['email'] ?? 'Nao informado';
+$_POST['password'] = isset($_POST['password']) ? $_POST['password'] : 'Nao informado';
 
 echo "o seu email é ". $_POST['email'];
-
-echo"seu email é {$_POST['email']}";
+echo"<br><br>";
+//echo"seu email é {$_POST['email']}";
+echo"sua senha é {$_POST['password']}";
 
 
 
